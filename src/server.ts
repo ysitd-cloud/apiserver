a0da0eabe4f7e7c5609cbb481a80a9a40943a5e4
Tony Yip <tony@opensource.hk>
2017-12-13 04:03:20 +0800

    Add security middleware

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { ApplicationModule } from './app/app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as morgan from 'morgan';
+import * as helmet from 'helmet';
+import * as express from 'express';
 
 (async () => {
   const options = new DocumentBuilder()
@@ -8,8 +11,13 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
     .setDescription('YSITD Cloud API Document')
     .setVersion('1.0')
     .build();
-  const app = await NestFactory.create(ApplicationModule);
+
+  const e = express();
+  e.use(morgan('dev'));
+  e.use(helmet());
+
+  const app = await NestFactory.create(ApplicationModule, e);
   const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('/openapi', app, document);
+  SwaggerModule.setup('/swagger', app, document);
   await app.listen(parseInt(process.env.PORT, 10) || 8080);
 })();
