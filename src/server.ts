33ec9e2c0340ac7286c651a598dccc923b54a8a8
Tony Yip <tony@opensource.hk>
2017-12-13 03:41:06 +0800

    Add OpenAPI

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { ApplicationModule } from './app/app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 (async () => {
+  const options = new DocumentBuilder()
+    .setTitle('YSITD Cloud API')
+    .setDescription('YSITD Cloud API Document')
+    .setVersion('1.0')
+    .build();
   const app = await NestFactory.create(ApplicationModule);
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('/openapi', app, document);
   await app.listen(parseInt(process.env.PORT, 10) || 8080);
 })();
