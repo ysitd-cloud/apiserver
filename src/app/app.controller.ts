f0c7c782c7ffb7978d07de0a60fcc604af395057
Tony Yip <tony@opensource.hk>
2017-12-15 02:10:18 +0800

    Add API endpoint list

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,8 +1,31 @@
 import { Controller, Get, Res } from '@nestjs/common';
-import { ApiOperation, ApiProduces, ApiResponse } from '@nestjs/swagger';
+import { ApiModelProperty, ApiOperation, ApiProduces, ApiResponse } from '@nestjs/swagger';
+
+class Endpoint {
+  @ApiModelProperty({ required: true, type: String, isArray: true, description: 'List of API endpoint'})
+  readonly path: string[]
+}
 
 @Controller()
 export class AppController {
+
+  @Get('/')
+  @ApiOperation({
+    title: 'List API endpoint',
+  })
+  @ApiResponse({ status: 200, description: 'Getting List of API endpoint', type: Endpoint })
+  getPath(): { path: string[] } {
+    return {
+      path: [
+        '/ping',
+
+        '/graphql',
+        '/swagger',
+
+        '/app',
+      ].sort(),
+    }
+  }
 
   @Get('ping')
   @ApiOperation({
