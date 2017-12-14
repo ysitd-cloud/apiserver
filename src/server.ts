2d15fb2943ccd1db3a3b2abe27d240ab21be4dfd
Tony Yip <tony@opensource.hk>
2017-12-15 00:03:53 +0800

    Add dotenv integration

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,7 @@
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
 import { NestFactory } from '@nestjs/core';
 import { ApplicationModule } from './app/app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
