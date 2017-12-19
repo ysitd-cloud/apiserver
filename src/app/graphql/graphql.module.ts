f21fc33a11caa15df9fcc63808180a9384f30a36
Tony Yip <tony@opensource.hk>
2017-12-20 02:54:31 +0800

    Fix for tslint

--- src/app/graphql/graphql.module.ts
+++ src/app/graphql/graphql.module.ts
@@ -1,10 +1,10 @@
 import { MiddlewaresConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { graphiqlExpress } from 'apollo-server-express';
+import { AccountModule } from '../account/account.module';
 import { GraphQLMiddleware } from './graphql.middleware';
 import { GraphQLService } from './graphql.service';
 import { ResolveModule } from './resolver/resolver.module';
 import { TokenMiddleware } from './token.middleware';
-import { AccountModule } from '../account/account.module';
 
 @Module({
   modules: [ResolveModule, AccountModule],
