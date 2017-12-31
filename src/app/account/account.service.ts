f5b36a27aadac27b9919caa75c0af67412cbf1e5
Tony Yip <tony@opensource.hk>
2017-12-31 21:58:26 +0800

    Use RESTful instead of grpc

--- src/app/account/account.service.ts
+++ src/app/account/account.service.ts
@@ -1,12 +1,6 @@
 import { Component } from '@nestjs/common';
-import { credentials } from 'grpc';
-import {
-  GetTokenInfoReply,
-  GetTokenInfoRequest,
-  GetUserInfoReply,
-  GetUserInfoRequest,
-} from 'ysitd-cloud-interfaces/account/actions/account_pb';
-import { AccountClient } from 'ysitd-cloud-interfaces/account/grpc_grpc_pb';
+import axios from 'axios';
+import * as moment from 'moment';
 import { ConfigService } from '../config/config.service';
 import { GrpcTransformService } from './grpc.transform.service';
 import { Token as IToken, User as IUser } from './interfaces';
@@ -16,45 +10,30 @@ export class AccountService {
   constructor(private readonly config: ConfigService, private readonly transform: GrpcTransformService) {}
 
   async getUserInfo(username: string): Promise<IUser | null> {
-    const client = this.getClient();
-    const req = new GetUserInfoRequest();
-    req.setUsername(username);
-    return new Promise<IUser>((resolve, reject) => {
-      client.getUserInfo(req, (e: Error | null, reply: GetUserInfoReply) => {
-        client.close();
-        if (e) {
-          reject(e);
-        } else {
-          if (!reply.getExists()) {
-            resolve(null);
-          } else {
-            resolve(this.transform.decodeUser(reply.getUser()));
-          }
-        }
-      });
-    });
+    const { data } = await axios.get(`http://${this.getHostname()}/user/${username}`);
+    if (!data.exists) {
+      return null;
+    }
+
+    return data.user as IUser;
   }
 
-  async getTokenInfo(token: string): Promise<IToken | null> {
-    const client = this.getClient();
-    const req = new GetTokenInfoRequest();
-    req.setToken(token);
-    return new Promise<IToken>((resolve, reject) => {
-      client.getTokenInfo(req, (e: Error | null, reply: GetTokenInfoReply) => {
-        client.close();
-        if (e) {
-          reject(e);
-          return;
-        } else if (!reply.getExists()) {
-          resolve(null);
-        }
+  async getTokenInfo(code: string): Promise<IToken | null> {
+    const { data } = await axios.get(`http://${this.getHostname()}/token/${code}`);
+    if (!data.exists) {
+      return null;
+    }
 
-        resolve(this.transform.decodeToken(reply.getToken()));
-      });
-    });
+    const { token } = data;
+    if ('expire' in token && token.expire) {
+        const timestamp = token.expire;
+        token.expire = moment(timestamp.seconds * 1000 + timestamp.nanos);
+    }
+
+    return token as IToken;
   }
 
-  private getClient(): AccountClient {
-    return new AccountClient(this.config.get('account.endpoint'), credentials.createInsecure());
+  private getHostname(): string {
+      return this.config.get('account.endpoint');
   }
 }
