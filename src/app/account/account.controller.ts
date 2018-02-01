765b7ef2a9348d462a523efdc85a71def478782e
Tony Yip <tony@opensource.hk>
2018-02-01 21:42:19 +0800

    Fix for rxjs

--- src/app/account/account.controller.ts
+++ src/app/account/account.controller.ts
@@ -1,7 +1,6 @@
 import { Controller, Get, NotFoundException, Param, UseGuards } from '@nestjs/common';
 import { ApiBearerAuth, ApiOperation, ApiResponse, ApiUseTags} from '@nestjs/swagger';
-import { Observable } from 'rxjs';
-import { ErrorObservable } from 'rxjs/observable/ErrorObservable';
+import { Observable } from 'rxjs/Observable';
 import { DeployerService } from '../deployer/deployer.service';
 import { UserApp } from '../deployer/interfaces';
 import { Application } from '../deployer/swagger';
@@ -25,13 +24,13 @@ export class AccountController {
   })
   @ApiResponse({ status: 200, description: 'Get user information', type: User })
   @ApiResponse({ status: 404, description: 'User not exists' })
-  getUserInfo(@Param('username') username: string): Observable<User | ErrorObservable> {
+  getUserInfo(@Param('username') username: string): Observable<User> {
     return this.service.getUserInfo(username)
       .map((user) => {
         if (user !== null) {
           return user;
         } else {
-          return Observable.throw(new NotFoundException());
+          throw new NotFoundException();
         }
       });
   }
