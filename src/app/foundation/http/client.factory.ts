faaab0891759606490ed87deb20eedad967b1b26
Tony Yip <tony@opensource.hk>
2018-02-01 17:05:03 +0800

    Change validate status for http

--- src/app/foundation/http/client.factory.ts
+++ src/app/foundation/http/client.factory.ts
@@ -3,6 +3,10 @@ import axios from 'axios';
 export const ClientFactory = {
   provide: 'client',
   useFactory() {
-    return axios.create();
+    return axios.create({
+      validateStatus(status) {
+        return status >= 200 && status < 600;
+      },
+    });
   },
 };
